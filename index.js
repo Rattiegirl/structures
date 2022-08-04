e15334bde3078d683222631bc63810a0b9c2812e
RattieGirl <zlatogriviy.zherebyonok@gmail.com>
2022-08-04 09:03:06 -0700

    Number array part 1

---
+++ index.js
@@ -0,0 +1,7 @@
+const {max,min,avg} = require("./lib/NumberArray")
+
+const arr = [1, 5, 3, -4, 2, 5,  43, 67, 4, 8, 4, 3, 8]
+
+//////////console.log(max(arr))
+//////////console.log(min(arr))
+console.log(avg(arr))
