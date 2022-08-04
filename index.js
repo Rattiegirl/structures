19086aad90773217021c43e49beb3573c9255f8a
RattieGirl <zlatogriviy.zherebyonok@gmail.com>
2022-08-04 09:34:04 -0700

    GetDictionary returned help part 2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const {max,min,avg} = require("./lib/NumberArray")
+const {max,min,avg, filter, count, getDictionary} = require("./lib/NumberArray")
 
 const arr = [1, 5, 3, -4, 2, 5,  43, 67, 4, 8, 4, 3, 8]
 
 //////////console.log(max(arr))
 //////////console.log(min(arr))
-console.log(avg(arr))
+//////////console.log(avg(arr))
+///////////console.log(filter(arr, "even"))
+///////////console.log(filter(arr, "odd"))
+///////////console.log(filter(arr, "div", 6))
+///////////console.log(count(arr , 3))
+console.log(getDictionary(arr))
